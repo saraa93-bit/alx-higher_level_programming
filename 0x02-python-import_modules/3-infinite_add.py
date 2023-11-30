4682f363d8288d2f41d6ac599d31f8f813d0476f
saraa93-bit <saraa93177228@gmail.com>
2023-11-30 19:27:10 +0200

    updates

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    # Exclude the script name and convert each argument to an integer
+    args_as_int = [int(arg) for arg in argv[1:]]
+
+    # Calculate the sum of all integers in the arguments
+    result = sum(args_as_int)
+
+    # Print the result followed by a new line
+    print(result)
