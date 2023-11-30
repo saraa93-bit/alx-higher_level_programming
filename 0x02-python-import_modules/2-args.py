f819832965a857a2b49196ec7cf96629e479a943
saraa93-bit <saraa93177228@gmail.com>
2023-11-30 19:24:34 +0200

    updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    num_args = len(argv) - 1
+
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        print("{:d} argument{}:".format(num_args, "s" if num_args > 1 else ""))
+        for i in range(1, num_args + 1):
+            print("{:d}: {}".format(i, argv[i]))
