959b7f06b2bb8406ec7857f2c21e4c0084bc86b0
saraa93-bit <saraa93177228@gmail.com>
2023-12-01 17:40:28 +0200

    updates

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import dis
-
-with open("hidden_4.pyc", "rb") as file:
-    code = file.read()
-
-dis.dis(code)
+if __name__ == "__main__":
+    import hidden_4
+    for i in dir(hidden_4):
+        if i != "__":
+            print(i)
+    
