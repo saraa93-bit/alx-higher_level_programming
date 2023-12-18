511ebede849b5bb73cde2158ea3d7f4216540b7e
saraa93-bit <saraa93177228@gmail.com>
2023-12-18 20:18:23 +0200

    py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        import sys
+        print("Exception:", e, file=sys.stderr)
+        return False
