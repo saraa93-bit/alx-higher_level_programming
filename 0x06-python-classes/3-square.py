9f5fbaf23d4be594d5d8e90a160156f6dd5e35e3
saraa93-bit <saraa93177228@gmail.com>
2023-12-19 10:21:59 +0200

    python

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,13 @@
+class Square:
+    def __init__(self, size=0):
+        self.__size = self.__validate_size(size)
+
+    def __validate_size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        return size
+
+    def area(self):
+        return self.__size ** 2
