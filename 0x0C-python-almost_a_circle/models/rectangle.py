5f8238705527d45c4c8c473ef1c557783beace7f
saraa93-bit <saraa93177228@gmail.com>
2024-01-15 19:52:23 +0200

    py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''Class Rectangle'''
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''
+        Defines the Rectangle class
+        Inherits from Base.
+    '''
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+        
+         @property
+    def width(self):
+        '''
+            Return private attribute
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        '''
+            Returning private attribute
+        '''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("height", value)
+        self.__height = value
+        
+        
+        
+        
+    
