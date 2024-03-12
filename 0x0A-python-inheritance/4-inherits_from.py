50568d2526adfce50e3dc6e85b426c3e0bb1d9d2
saraa93-bit <saraa93177228@gmail.com>
2024-03-12 12:02:38 +0200

    js

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,6 +12,4 @@ def inherits_from(obj, a_class):
         If obj is an inherited instance of a_class - True.
         Otherwise - False.
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    return issubclass(type(obj), a_class) and type(obj) != a_class:
---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Print string “Javascript is amazing”
+
+const myvar = "JavaScript is amazing";
+console.log(myvar);
