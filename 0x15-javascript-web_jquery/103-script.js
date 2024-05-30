fc6fac24ce338de89a266d4b4916b7c2664dd2c6
saraa93-bit <saraa93177228@gmail.com>
2024-05-30 12:04:48 +0300

    jquery

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#btn_translate').click(function(){
+        const languageCode = $('#language_code').val();
+        $.get(`https://www.fourtonfish.com/hellosalut/?lang=${languageCode}`, function(data){
+            $('#hello').text(data.hello);
+        });
+    });
+
+    $('#language_code').keypress(function(event){
+        if (event.which == 13) {
+            $('#btn_translate').click();
+        }
+    });
+});
